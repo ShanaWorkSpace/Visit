454b6a9159f6cdf8be5535097fbdbbdc1494178d
Shana Yasmin S <shanayasminofficial@gmail.com>
2025-02-18 23:07:32 +0530

    manager logged in

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,16 +1,44 @@
 package pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage{
-	WebDriver driver;
+	private WebDriver driver;
 	
 	public LoginPage(WebDriver driver)
 	{
 		this.driver = driver;
 	}
-	
+	public void titleCheck() {
+		 WebElement pass = driver.findElement(By.xpath("//h2[contains(text(), 'Welcome Back!')]"));
+		 pass.getText();
+	 }
+	 public void enterUsername() {
+		 WebElement user = driver.findElement(By.xpath("//input[@name='userId']"));
+		 user.sendKeys("manager1@ictkerala.org");
+	 }
 		
+	 public void enterPassword() {
+		 WebElement pass = driver.findElement(By.xpath("//input[@name='password' and @placeholder='Password']"));
+		 pass.sendKeys("@Manager#952");
+	 }
+	 
+	 public void eye() {
+		 WebElement eye = driver.findElement(By.xpath("//input[@name='password' and @placeholder='Password']"));
+		 eye.click();
+	 }
+	 
+	 public void signIn() {
+		 WebElement signin = driver.findElement(By.xpath("//button[contains(text(),'Sign In') and contains(@class, 'bg-[#32b1dc]')]"));
+		 signin.click();
+	 }
+	 
 }
 
 
--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,14 +1,22 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import base.TestBase;
 import pages.LoginPage;
 
 public class LoginPageTest extends TestBase{
+	
 	LoginPage log;
+	
 	
 	@Test
 	public void testURL() {
@@ -16,4 +24,17 @@ public class LoginPageTest extends TestBase{
     Assert.assertEquals(currentDashUrl, "https://dev.visit.ictkerala.org/");
     System.out.println("Test:URL Verification passed");
 	}
+	
+	@Test
+	public void Login_Manager() {
+	log = new LoginPage(driver);
+	log.titleCheck();
+	log.enterUsername();
+	log.enterPassword();
+	log.eye();
+	log.signIn();
+	
+	System.out.println("Test:Manager Login Verification passed");
+	}
+	
 }
